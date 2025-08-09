c21a302d8ed1b3c4d32a8c4ce0a43dde010462de
Karen Collins <fisheskc@googlemail.com>
2025-08-09 22:23:24 +0100

    Updated the layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     // <ClerkProvider dynamic>
-    <ClerkProvider>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
     <html lang='en' suppressHydrationWarning>
         <body className={inter.className}>
           <Providers>
